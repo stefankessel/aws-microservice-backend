f9bdf911768fb46c075d20dd8c573c0bcf3f5435
Stefan Kessel <kessel.stefan1@gmail.com>
2023-10-29 15:36:18 +0100

    product-lambda cdk

--- lib/cdk-microservice-stack.ts
+++ lib/cdk-microservice-stack.ts
@@ -1,7 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
+const PRIMARY_KEY = 'id'
 
 export class CdkMicroserviceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +13,7 @@ export class CdkMicroserviceStack extends cdk.Stack {
 
     const productTable = new Table(this, 'product', {
       partitionKey: {
-        name: 'id',
+        name: PRIMARY_KEY,
         type: AttributeType.STRING
       },
       billingMode: BillingMode.PAY_PER_REQUEST,
@@ -21,8 +25,27 @@ export class CdkMicroserviceStack extends cdk.Stack {
 
     // create NodeFunction
     // props in NodeFunctionProps
-    // give lambda permission to access dynamodb table
 
-    
+    const lambdaProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ['aws-sdk'],
+        
+      },
+      runtime: Runtime.NODEJS_18_X,
+      environment: {
+        PRIMARY_KEY,
+        TABLE_NAME: productTable.tableName
+      }
+    }
+
+    const productLambda = new NodejsFunction(this, 'productLambda', {
+      entry: join(__dirname, '../src/product/index.js'),
+      ...lambdaProps,
+    })
+
+    // give lambda permission to access dynamodb table
+    productTable.grantReadWriteData(productLambda)
+
+
   }
 }
