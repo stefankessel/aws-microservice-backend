d4a6f0e99497d0c9c122cce393836b69e2af6e99
Stefan Kessel <kessel.stefan1@gmail.com>
2023-11-05 16:56:25 +0100

    productHandler/ added getItem

--- src/product/index.js
+++ src/product/index.js
@@ -1,11 +1,50 @@
+import { GetItemCommand } from "@aws-sdk/client-dynamodb"; 
+import {client} from './ddbClient'
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
 exports.handler = async (event) => {
     console.log('request: ', JSON.stringify(event, undefined, 2))
 
-    const res = {
+    const httpMethod = event.httpMethod
+
+    switch (httpMethod) {
+        case 'GET':
+            if( event.pathParameters != null){
+                body =  await getProduct(event.pathParameters)
+            }
+            else{
+                body =  await getAllProducts()
+            }
+            
+        // default:
+        //     throw new Error(`Unsupported method "${httpMethod}"`)
+    
+    }
+
+    return{
         statusCode: 200,
         body : `Hello from Lambda, Path: ${event.path}\n`
     }
 
-    return res
+   
+}
+
+async function getProduct(productId){
+    console.log('getProduct Function')
+
+    try {
+        const params ={
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: {
+              id: marshall({id: productId})
+            },
+        }
+        const {item} = client.send(new GetItemCommand(params))
+
+        return item ? unmarshall(item) : {}
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+    
 }
